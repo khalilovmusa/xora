b324b0334d133effe88e2723556087347e9ebd05
Musa <Musaxlilov29@gmail.com>
2025-03-13 23:10:22 +0400

    Button add

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,19 +1,24 @@
 import clsx from 'clsx'
 import React from 'react'
+import Marker from './Marker'
 
-const Button = ({ icon, children, href, containerClassName, onClick }) => {
+const Button = ({ icon, children, href, containerClassName, onClick, markerFill }) => {
     const Inner = () => (
         <>
-        <span>
-            <span>Marker</span>
+        <span className='relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden'>
+            <span className='absolute -left-[1px]'>
+                <Marker markerFill={markerFill} />
+            </span>
             {icon && (
                 <img src={icon} alt="circle" className='size-10 mr-5 object-container z-10'/>
             )}
 
-            <span>
+            <span className='relative z-2 font-poppins base-bold text-p1 uppercase'>
                 {children}
             </span>
         </span>
+
+        <span className='glow-before glow-after'/>
         </>
     )
 
