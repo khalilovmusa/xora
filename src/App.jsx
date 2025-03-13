b7140d38a92d1c21e6a8fe958dd56326348b893a
Musa <Musaxlilov29@gmail.com>
2025-03-13 04:06:59 +0400

    Header section add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/molecules/Header'
+import Hero from './components/molecules/Hero'
 
 const App = () => {
 
   return (
     <main className='overflow-hidden'>
       <Header />
+      <Hero />
     </main>
   )
 }
